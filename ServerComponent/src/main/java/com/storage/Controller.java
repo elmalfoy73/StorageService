fa8562403adb0fadf7d0395d0bec87814edf9f54
Elizabeth Malfoy <elmalfoy73@gmail.com>
2023-10-11 16:36:28 +0300

    fix2: Controller

--- ServerComponent/src/main/java/com/storage/Controller.java
+++ ServerComponent/src/main/java/com/storage/Controller.java
@@ -3,22 +3,26 @@ package com.storage;
 import com.storage.Entities.KVPair;
 import com.storage.service.StorageComponentService;
 import com.storage.utils.KVPairRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/keys")
 public class Controller {
+
+    @Autowired
     KVPairRepository repository;
 
-    @GetMapping("/{key}")
+    @GetMapping("/get/{key}")
     public KVPair get(@PathVariable String key){
         return repository.findByKey(key);
     }
 
-    @PostMapping("/set/{key}/{value}")
-    public void set(@PathVariable String key, @PathVariable String value) {
+    @RequestMapping("/set/{key}/{value}")
+    public KVPair set(@PathVariable String key, @PathVariable String value) {
         KVPair pair = new KVPair(key, value);
         repository.save(pair);
+        return pair;
     }
 
 }
